8a12bd88b64e2820111478e045114f150cd0d357
wiz <y676611165@163.com>
2022-11-14 11:03:07 +0800

    feat: 添加链表

---
+++ src/leetcode83.c
@@ -0,0 +1,44 @@
+#include <test.h>
+#include <stdlib.h>
+#include <stdio.h>
+// #链表
+struct ListNode* deleteDuplicates(struct ListNode* head){
+	struct ListNode *ans=head,*cur=head;
+	if (head==NULL)
+	{
+		return ans;
+	}
+	
+	head=head->next;
+	while (head!=NULL)
+	{
+		if (head->val==cur->val)
+		{
+			cur->next=head->next;
+		}
+		else
+		{
+			cur=head;
+		}
+		head=head->next;
+	}
+	return ans;
+}
+
+
+int main(){
+	int a[]={1,1,2,2};
+	int b[]={1,1,2,3,3,4};
+	int c[]={1,2,2,3};
+	struct ListNode *ans;
+	ans=init(a,4);
+	ans=deleteDuplicates(ans);
+	listprint(ans);
+	ans=init(b,6);
+	ans=deleteDuplicates(ans);
+	listprint(ans);
+	ans=init(c,4);
+	ans=deleteDuplicates(ans);
+	listprint(ans);
+	
+}
