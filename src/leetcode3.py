307f7fc26034e9d6f504e31763b945f578db6924
wiz <y676611165@163.com>
2022-11-16 16:36:54 +0800

    feat: 自己重新写一份

--- src/leetcode3.py
+++ src/leetcode3.py
@@ -2,18 +2,21 @@
 class Solution:
 	def lengthOfLongestSubstring(self, s: str) -> int:
 		if not s:return 0
-		b = 0
-		win = set()
-		n = len(s)
-		max_len = 0
-		cur_len = 0
-		for i in range(n):
-			cur_len += 1
-			while (s[i] in win):
-				win.remove(s[b])
-				b += 1
-				cur_len -= 1
-			if cur_len > max_len:
-				max_len = cur_len
+		win=set()
+		winlenmax=0
+		left=0
+		for i in range(len(s)):
+			while s[i] in win:
+				winlenmax=max([winlenmax,len(win)])
+				win.remove(s[left])
+				left+=1
 			win.add(s[i])
-		return max_len
+		while s[i] in win:
+			winlenmax=max([winlenmax,len(win)])
+			win.remove(s[left])
+			left+=1
+		return winlenmax
+
+if __name__ == "__main__":
+	s=Solution().lengthOfLongestSubstring
+	print(s("pwwkew"))
