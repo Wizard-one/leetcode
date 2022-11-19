d7b49fd52ce5f0364835c2581f4844e00ea74672
wiz <y676611165@163.com>
2022-11-19 10:57:13 +0800

    chore: 添加注释

--- src/leetcode701.c
+++ src/leetcode701.c
@@ -1,6 +1,14 @@
 #include <test.h>
 
 // #二叉树 #迭代 [[leetcode700]] #BST
+
+/**
+ * @brief 二叉搜索树的插入
+ * 
+ * @param root 被插入的BST
+ * @param val 被插入的val
+ * @return struct TreeNode* 
+ */
 struct TreeNode* insertIntoBST(struct TreeNode* root, int val){
 	struct TreeNode *ans=malloc(sizeof(struct TreeNode)),*cur=root;
     
@@ -11,12 +19,15 @@ struct TreeNode* insertIntoBST(struct TreeNode* root, int val){
         return ans;
 	while (1)
 	{
+		// 当前val 小于目标，说明应该插入到右侧
 		if (cur->val<val)
 		{
+			/* 右侧非空说明还有值继续向右搜索 */
 			if (cur->right!=NULL)
 			{
 				cur=cur->right;
 			}
+			/* 右侧空了说明可以插入 */
 			else
 			{
 				cur->right=ans;
@@ -25,6 +36,7 @@ struct TreeNode* insertIntoBST(struct TreeNode* root, int val){
 		}
 		else
 		{
+			// 当前val 小于目标，说明应该插入到左侧
 			if (cur->left!=NULL)
 				cur=cur->left;
 			else
