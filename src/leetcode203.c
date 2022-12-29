dd95c2c12fc943538202da7603ad87665e6daa44
wiz <y676611165@163.com>
2022-12-29 10:30:39 +0800

    docs: 添加注释

--- src/leetcode203.c
+++ src/leetcode203.c
@@ -2,12 +2,20 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdbool.h>
+
+
+/* 203. 移除链表元素
+
+给你一个链表的头节点 head 和一个整数 val ，请你删除链表中所有满足 Node.val == val 的节点，并返回 新的头节点 。  */
+
+
 // #链表 #递归
 struct ListNode* removeElements(struct ListNode* head, int val){
 	if (head==NULL)
 	{
 		return NULL;
 	}
+	// 先递归到链表最后一个节点，从后向前删除，递归使用的栈就记录了整个链表所有信息
 	head->next=removeElements(head->next,val);
 	if (head->val==val)
 	{
--- src/leetcode217.py
+++ src/leetcode217.py
@@ -1,4 +1,13 @@
 from typing import List
+
+
+""" 217. 存在重复元素
+给你一个整数数组 nums 。如果任一值在数组中出现 至少两次 ，返回 true ；如果数组中每个元素互不相同，返回 false 。 
+ """
+
+# #hash #排序
+
+""" 排序很显然，hash表也很显然，使用 collections.Counter 即可 """
 def containsDuplicate(self, nums: List[int]) -> bool:
 	nums=nums.sort()
 	a=nums[0]
