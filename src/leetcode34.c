c3a4d295001368db395f286fa1c37eb94063b37e
wiz <y676611165@163.com>
2023-01-19 16:23:01 +0800

    feat: 更新二分法

--- src/leetcode34.c
+++ src/leetcode34.c
@@ -9,86 +9,51 @@
 
 // #二分法 #排序 #搜索 
 
-typedef struct range
-{
-	int l,r;
-}range;
 
-/**
- * @brief 获取插入搜索范围
- * 
- * 使用递归的二分搜索来解决问题
- * 
- * @param a 搜索数组
- * @param t 搜索目标
- * @param rb 右侧最大边界
- * @param l 左边界
- * @param r 右边界
- * @return range 
- */
-range get(int* a,int t,int l,int r)
-{
-	int m=(l+r)>>1;
-	// 两个终止条件
-	if (a[m]==t)
-	{
-		// 目标值找到返回
-		return (range){m,m};
-	}
-	else if (a[m+1]>t && a[m]<t)
-	{
-		// 目标值不在列表中返回插入位置
-		return (range){m+1,m+1};
-	}	
-	else if (a[m]>t)
-	{
-		// 向左递归二分查找
-		return get(a,t,l,m);
-	}
-	else
-	{
-		// 向右递归二分查找
-		return get(a,t,m+1,r);
-	}
-	 
-}
+/* 先通过二分法确定位置,后向左右延拓, 但是延拓相对来说还可以使用二分法加速
 
-/* 先通过二分法确定位置,后向左右延拓
-
-官方解答是通过两次二分法来实现,先找到第一个大于等于target left 然后再从left开始找第一个小于等于target 的 right
+所以官方解答是通过两次二分法来实现,先找到第一个大于等于target left 然后再从left开始找第一个小于等于target 的 right
 
  */
 int* searchRange(int* nums, int numsSize, int target, int* returnSize){
-	*returnSize=2;
+	*(returnSize)=2;
 	int *ans;
 	ans=malloc(sizeof(int)*2);
-	// 排序数组为空,target超出数组范围的情况
-	if (numsSize==0 || nums[0]> target || nums[numsSize-1]<target)
+	int l=0,r=numsSize,m;
+	// 先搜索左边界
+	while (l<r)
+	{
+		m=l+(r-l)/2;
+		if (nums[m]==target)
+			r=m;
+		else if (nums[m]>target)
+			r=m;
+		else
+			l=m+1;
+	}
+	if (l==numsSize || nums[l]!=target)
 	{
 		ans[0]=-1;
 		ans[1]=-1;
 		return ans;
 	}
-	// 二分查找找到位置
-	int loc=get(nums,target,0,numsSize-1).l;
-	// 排除没有找到的情况
-	if (nums[loc]!=target)
+		
+	ans[0]=l;
+	// 然后搜索右边界
+	r=numsSize;
+	while (l<r)
 	{
-		ans[0]=-1;
-		ans[1]=-1;
-		return ans;
+		m=l+(r-l)/2;
+		if (nums[m]==target)
+			l=m+1;
+		else if (nums[m]>target)
+			r=m;
+		else
+			l=m+1;
 	}
-	// 左右延拓
-	int i=loc+1;
-	while (i<numsSize && nums[i]==target)
-		i++;
-	ans[1]=i-1;
-
-	i=loc-1;
-	while (i>=0 && nums[i]==target)
-		i--;
-	ans[0]=i+1;
+	ans[1]=l-1;
 	return ans;
+	
 }
 
 int main(){
--- src/leetcode704.c
+++ src/leetcode704.c
@@ -1,24 +1,23 @@
 #include <stdlib.h>
 #include <stdio.h>
+
+/* 704. 二分查找
+
+给定一个 n 个元素有序的（升序）整型数组 nums 和一个目标值 target  ，写一个函数搜索 nums 中的 target，如果目标值存在返回下标，否则返回 -1。
+ */
+
 int search(int* nums, int numsSize, int target){
 	int m;
 	int l=0,r=numsSize-1;
-	m=(l+r)/2;
 	while (l<=r)
 	{
+		m=l+(l-r)/2;//有效的防止数据溢出
 		if(target==nums[m])
-		{
 			return m;
-		}
 		else if (target>nums[m])
-		{
 			l=m+1;
-		}
 		else
-		{
 			r=m-1;
-		}
-		m=(l+r)/2;
 	}
 	return -1;
 }
